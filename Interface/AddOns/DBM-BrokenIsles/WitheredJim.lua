88b214b49938be3fbfdac779206fc750e492520a
shuwangyou <136198044@qq.com>
2020-11-19 19:53:11 +0800

    commit

--- Interface/AddOns/DBM-BrokenIsles/WitheredJim.lua
+++
@@ -1,9 +0,0 @@
-local mod	= DBM:NewMod(1796, "DBM-BrokenIsles", 1, 822)
-local L		= mod:GetLocalizedStrings()
-
-mod:SetRevision("20201111164513")
-mod:SetCreatureID(102075)--112350
---mod:SetEncounterID(1880)
-mod:SetReCombatTime(20)
-
-mod:RegisterCombat("combat")
